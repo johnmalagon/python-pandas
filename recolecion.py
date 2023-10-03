0e0d1894428d1ed6517ee74cd86b6a0e167c20c5
johnMalagon <johnalexandermalagongarcia@gmail.com>
2023-10-03 10:35:15 -0500

    pandas bien

---
+++ recolecion.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler,
+StandardScaler
+# Cargar el conjunto de datos
+# data = pd.read_csv("ruta_del_archivo.csv") #
+Reemplazar "ruta_del_archivo.csv" por la ubicación
+de tu archivo de datos
+# Normalización
+scaler = MinMaxScaler() # Inicializar el objeto
+MinMaxScaler
+data_normalized = scaler.fit_transform(data) #
+Aplicar la normalización a los datos
+# Estandarización
+scaler = StandardScaler() # Inicializar el objeto
+StandardScaler
+data_standardized = scaler.fit_transform(data) #
+Aplicar la estandarización a los datos
+# Agregación
+aggregated_data =
+data.groupby("columna_agrupadora").agg({"columna1":
+"sum", "columna2": "mean", "columna3": "max"})
+# Reemplazar "columna_agrupadora", "columna1",
+"columna2" y "columna3" por los nombres de las
+columnas relevantes en tu conjunto de datos
